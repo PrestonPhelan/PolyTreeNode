2d9b18cad79c82f501b0814f35a3fc86687fb4be
App Academy Student <student@appacademy.io>
2016-11-18 10:52:49 -0800

    Added initialize & parent= methods to PolyTreeNode

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,20 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @parent = nil
+    @children = Array.new
+    @value = value
+  end
+
+  def parent=(node)
+    unless parent
+      ##Reassign parent/children
+    end
+
+    @parent = node
+    node.children << self if node && !node.children.include?(self)
+
+  end
 
 end
